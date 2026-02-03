20096cf4bb4b3f1d6bab06b048e756f86f96bca6
vitovodenko <vitovodenko@gmail.com>
2026-02-03 11:11:11 -0800

    Update default peer connection

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -137,7 +137,8 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("node3.example.com");
+        vSeeds.emplace_back("node1.iskandercoin.com");
+        vSeeds.emplace_back("node2.iskandercoin.com");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,45); // K addresses
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
